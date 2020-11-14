9f33c57ea3bfafeed0f6ac2d9b94cdbddbe98189
delhi09 <masataka.okudera.biz@gmail.com>
2020-11-15 04:13:38 +0900

    adminにmodelを登録

--- cookbook/ingredients/admin.py
+++ cookbook/ingredients/admin.py
@@ -1,3 +1,6 @@
+# cookbook/ingredients/admin.py
 from django.contrib import admin
+from ingredients.models import Category, Ingredient
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Ingredient)
